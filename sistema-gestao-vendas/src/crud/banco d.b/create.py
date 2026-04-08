4add9fd0e8b0695a34c97e1992d4fd04862c73e8
renancruz09 <renan.cruz09@aluno.ifce.edu.br>
2026-04-08 00:42:30 +0000

    atualização do banco de dados

--- sistema-gestao-vendas/src/crud/banco d.b/create.py
+++ sistema-gestao-vendas/src/crud/banco d.b/create.py
@@ -1,1 +1,15 @@
 import sqlite3
+
+def inicializar_banco ():
+    conexao = sqlite3.connect('banco.db/database.db')
+    cursor = conexao.cursor()
+
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS vendas (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    produto TEXT NOT NULL,
+                    quantidade INTEGER NOT NULL,
+                    valor REAL NOT NULL,
+                    data TEXT NOT NULL
+                   )
+''')
+                
